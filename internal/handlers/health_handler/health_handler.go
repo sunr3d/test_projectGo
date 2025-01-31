7108bb13dc181ffddc5626c2d497e578172b3711
Sunred <sunr3d.coding@gmail.com>
2025-01-31 14:14:19 +0300

    fix: add method "Watch" (HealtServer interface can't work w/o it)

--- internal/handlers/health_handler/health_handler.go
+++ internal/handlers/health_handler/health_handler.go
@@ -14,3 +14,9 @@ func NewHealthService() *HealthService {
 func (h *HealthService) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
 	return &grpc_health_v1.HealthCheckResponse{Status: grpc_health_v1.HealthCheckResponse_SERVING}, nil
 }
+
+func (s *HealthService) Watch(req *grpc_health_v1.HealthCheckRequest, server grpc_health_v1.Health_WatchServer) error {
+	return server.Send(&grpc_health_v1.HealthCheckResponse{
+		Status: grpc_health_v1.HealthCheckResponse_SERVING,
+	})
+}
