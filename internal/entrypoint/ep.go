0bd331314cf9b20768f0faca8ded20b1481f000b
Sunred <sunr3d.coding@gmail.com>
2025-01-31 13:44:19 +0300

    chore: small fixes for logger

--- internal/entrypoint/ep.go
+++ internal/entrypoint/ep.go
@@ -2,16 +2,17 @@ package entrypoint
 
 import (
 	"fmt"
-	"go.uber.org/zap"
+	"go.uber.org/zap" // logger
 	"google.golang.org/grpc"
 	"link_service/internal/config"
+	//"link_service/internal/handlers"
 	"net"
 )
 
 func Run(cfg *config.Config, logger *zap.Logger) error {
 	grpcServer := grpc.NewServer()
 
-	// TODO: Сервисы, первый должен быть HealthCheck
+	// TODO: Регистрация сервисов, первый должен быть HealthCheck
 
 	address := fmt.Sprintf(":%s", cfg.GRPCPort)
 	listener, err := net.Listen("tcp", address)
@@ -19,15 +20,14 @@ func Run(cfg *config.Config, logger *zap.Logger) error {
 		return fmt.Errorf("failed to listen on address %s: %w\n", address, err)
 	}
 
-	logger.Info("start gRPC server",
-		zap.String("port", cfg.GRPCPort),
-		zap.String("version", cfg.Version),
-	)
-
 	if err = grpcServer.Serve(listener); err != nil {
 		return fmt.Errorf("failed to serve: %w", err)
 	}
-
+	logger.Info("gRPC server started successfully",
+		zap.String("address", "localhost:"),
+		zap.String("port", cfg.GRPCPort),
+		zap.String("version", cfg.Version),
+	)
 	return nil
 
 }
