561bfe694e2e72bbc188346318f8e70c389474fb
Kwaltz Areo <kwaltzar@student.21-school.ru>
2025-01-31 13:21:29 +0300

    feat: gRPC server realization

--- go.mod
+++ go.mod
@@ -6,6 +6,14 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	go.uber.org/zap v1.27.0
+	google.golang.org/grpc v1.70.0
 )
 
-require go.uber.org/multierr v1.10.0 // indirect
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
+)
--- internal/entrypoint/ep.go
+++ internal/entrypoint/ep.go
@@ -1,10 +1,33 @@
 package entrypoint
 
 import (
+	"fmt"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
 	"link_service/internal/config"
+	"net"
 )
 
 func Run(cfg *config.Config, logger *zap.Logger) error {
+	grpcServer := grpc.NewServer()
+
+	// TODO: Сервисы, первый должен быть HealthCheck
+
+	address := fmt.Sprintf(":%s", cfg.GRPCPort)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to listen on address %s: %w\n", address, err)
+	}
+
+	logger.Info("start gRPC server",
+		zap.String("port", cfg.GRPCPort),
+		zap.String("version", cfg.Version),
+	)
+
+	if err = grpcServer.Serve(listener); err != nil {
+		return fmt.Errorf("failed to serve: %w", err)
+	}
+
 	return nil
+
 }
