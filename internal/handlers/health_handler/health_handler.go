869594060c99e57285093ec450830df217838676
Sunred <sunr3d.coding@gmail.com>
2025-01-31 14:03:09 +0300

    feat: health handler

---
+++ internal/handlers/health_handler/health_handler.go
@@ -0,0 +1,16 @@
+package health_handler
+
+import (
+	"context"
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+type HealthService struct{}
+
+func NewHealthService() *HealthService {
+	return &HealthService{}
+}
+
+func (h *HealthService) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
+	return &grpc_health_v1.HealthCheckResponse{Status: grpc_health_v1.HealthCheckResponse_SERVING}, nil
+}
