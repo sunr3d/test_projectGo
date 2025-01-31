8ec57649f815f31b9185163e9559e3b93f031f66
Ivan <zakharovivann@gmail.com>
2025-01-31 17:50:21 +0300

    feat: Конструктор и Run, Stop методы для серверам

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,19 +1,43 @@
 package server
 
-import "google.golang.org/grpc"
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"net"
+)
 
 type Server struct {
-	server *grpc.Server
+	Server *grpc.Server
+	logger *zap.Logger
 }
 
-func NewServer() *Server {
+func NewServer(logger *zap.Logger) *Server {
 	return &Server{
-		server: grpc.NewServer(),
+		Server: grpc.NewServer(),
+		logger: logger,
 	}
 }
 
-func (s *Server) Run() {
+func (s *Server) Run(port string) error {
+	address := fmt.Sprintf(":%s", port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to listen on address %s: %w\n", address, err)
+	}
 
+	s.logger.Info("gRPC server start",
+		zap.String("address", address),
+	)
+
+	if err = s.Server.Serve(listener); err != nil {
+		return fmt.Errorf("failed to serve: %w", err)
+	}
+
+	return nil
 }
 
-func (s *Server) Stop() {}
+func (s *Server) Stop() {
+	s.logger.Info("gRPC server stop")
+	s.Server.GracefulStop()
+}
