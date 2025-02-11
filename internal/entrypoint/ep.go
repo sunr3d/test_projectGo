226e6b7855b59052175bd9cf5d2ec6ad247e5773
Sunred <sunr3d.coding@gmail.com>
2025-02-12 02:24:16 +0300

    chore: health handler > hh, link_service handler > lsh

--- internal/entrypoint/ep.go
+++ internal/entrypoint/ep.go
@@ -5,8 +5,8 @@ import (
 	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection" // reflection для теста ручек через `grpcurl` в терминале
 	"link_service/internal/config"
-	"link_service/internal/handlers/health_handler"
-	"link_service/internal/handlers/link_service"
+	hh "link_service/internal/handlers/health"
+	lsh "link_service/internal/handlers/link_service"
 	"link_service/internal/server"
 	pbls "link_service/proto/link_service"
 )
@@ -16,11 +16,11 @@ func Run(cfg *config.Config, logger *zap.Logger) error {
 	reflection.Register(grpcServer.Server) // reflection для теста ручек через `grpcurl` в терминале
 
 	// Регистрация сервиса health
-	healthService := health_handler.NewHealthService()
+	healthService := hh.NewHealthService()
 	grpc_health_v1.RegisterHealthServer(grpcServer.Server, healthService)
 
 	// Регистрация сервиса link_service
-	linkService := link_service.NewLinkService(nil) // placeholder
+	linkService := lsh.NewLinkService(nil) // placeholder
 	pbls.RegisterLinkServiceServer(grpcServer.Server, linkService)
 
 	// Запуск сервераЗ
