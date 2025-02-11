506ad2f69bea43d1d0928700a1c6902de546f297
Sunred <sunr3d.coding@gmail.com>
2025-02-11 23:49:05 +0300

    feat: InputLink method

--- internal/handlers/link_service/link_service.go
+++ internal/handlers/link_service/link_service.go
@@ -21,7 +21,22 @@ func (ls *LinkService) GetLink(ctx context.Context, req *pb.GetLinkRequest) (*pb
 }
 
 func (ls *LinkService) InputLink(ctx context.Context, req *pb.InputLinkRequest) (*pb.InputLinkResponse, error) {
-	// Реализация метода по добавлению данных в БД
-	id := "0" // PLACEHOLDER
-	return &pb.InputLinkResponse{Success: true, Message: "Link successfully added.", Id: id}, nil
+	inputLink := services.InputLink{
+		Link:      req.Link,
+		FakeLink:  req.FakeLink,
+		EraseTime: req.EraseTime.AsTime(),
+	}
+
+	if err := ls.service.Create(ctx, inputLink); err != nil {
+		return &pb.InputLinkResponse{
+			Success: false,
+			Message: err.Error(),
+			Id:      "",
+		}, err
+	}
+	return &pb.InputLinkResponse{
+		Success: true,
+		Message: "Link successfully added.",
+		Id:      "PLACEHOLDER_ID",
+	}, nil
 }
