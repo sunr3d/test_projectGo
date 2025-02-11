0564c59fa3eaea4238a7401f93a2a315be9f1b70
Sunred <sunr3d.coding@gmail.com>
2025-02-11 23:27:13 +0300

    feat: регистрация сервиса link_service на гРПС сервер (placeholder)

--- internal/entrypoint/ep.go
+++ internal/entrypoint/ep.go
@@ -6,7 +6,9 @@ import (
 	"google.golang.org/grpc/reflection" // reflection для теста ручек через `grpcurl` в терминале
 	"link_service/internal/config"
 	"link_service/internal/handlers/health_handler"
+	"link_service/internal/handlers/link_service"
 	"link_service/internal/server"
+	pbls "link_service/proto/link_service"
 )
 
 func Run(cfg *config.Config, logger *zap.Logger) error {
@@ -16,6 +18,10 @@ func Run(cfg *config.Config, logger *zap.Logger) error {
 	// Регистрация сервиса health
 	healthService := health_handler.NewHealthService()
 	grpc_health_v1.RegisterHealthServer(grpcServer.Server, healthService)
+
+	// Регистрация сервиса link_service
+	linkService := link_service.NewLinkService(nil) // placeholder
+	pbls.RegisterLinkServiceServer(grpcServer.Server, linkService)
 
 	// Запуск сервераЗ
 	if err := grpcServer.Run(cfg.GRPCPort); err != nil {
