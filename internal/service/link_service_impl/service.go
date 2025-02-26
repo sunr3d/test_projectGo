b179609d7916faa985ada5c51efb767f25f5e2d4
Sunred <sunr3d.coding@gmail.com>
2025-02-26 21:16:39 +0300

    feat: Изменение метода `Find()` под паттерн поиска данных из Кэша -> БД -> Кэширование

--- internal/service/link_service_impl/service.go
+++ internal/service/link_service_impl/service.go
@@ -13,10 +13,11 @@ var _ services.Service = (*service)(nil)
 type service struct {
 	logger *zap.Logger
 	repo   infra.Database
+	cache  infra.Cache
 }
 
-func New(logger *zap.Logger, repo infra.Database) services.Service {
-	return &service{logger: logger, repo: repo}
+func New(logger *zap.Logger, repo infra.Database, cache infra.Cache) services.Service {
+	return &service{logger: logger, repo: repo, cache: cache}
 }
 
 func (s *service) Create(ctx context.Context, link services.InputLink) error {
@@ -33,6 +34,14 @@ func (s *service) Create(ctx context.Context, link services.InputLink) error {
 }
 
 func (s *service) Find(ctx context.Context, fakeLink string) (string, error) {
+	// Ищем в кэше
+	cachedLink, err := s.cache.Get(ctx, fakeLink)
+	if err == nil && cachedLink != "" {
+		//fmt.Println("Link from cache: ", cachedLink) // TODO: DELETE DEBUG LINE
+		return cachedLink, nil
+	}
+
+	// Если в кэше нет, ищем в БД
 	link, err := s.repo.Find(ctx, fakeLink)
 	if err != nil {
 		return "", fmt.Errorf("failed to find link: %w", err)
@@ -40,5 +49,13 @@ func (s *service) Find(ctx context.Context, fakeLink string) (string, error) {
 	if link == nil {
 		return "", LinkNotFound
 	}
+	//fmt.Println("Link from DB: ", *link) // TODO: DELETE DEBUG LINE
+
+	// Сохраняем в кэш
+	if err = s.cache.Set(ctx, fakeLink, *link); err != nil {
+		return "", fmt.Errorf("failed to cache link: %w", err)
+	}
+	//fmt.Println("Saved to cache!", *link) // TODO: DELETE DEBUG LINE
+
 	return *link, nil
 }
