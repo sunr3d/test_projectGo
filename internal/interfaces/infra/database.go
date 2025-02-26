e392749e1c20ec55c90b496e18fd99023f27c70c
Sunred <sunr3d.coding@gmail.com>
2025-02-26 21:13:53 +0300

    feat: вынесение типов в отдельный файл

--- internal/interfaces/infra/database.go
+++ internal/interfaces/infra/database.go
@@ -2,17 +2,11 @@ package infra
 
 import (
 	"context"
-	"time"
 )
-
-type InputLink struct {
-	Link      string
-	FakeLink  string
-	EraseTime time.Time
-}
 
 //go:generate go run github.com/vektra/mockery/v2@v2.20.2 --name=Database --output=../../../mocks
 type Database interface {
 	Find(ctx context.Context, fakeLink string) (*string, error)
 	Create(ctx context.Context, link InputLink) error
+	Close() error
 }
--- internal/interfaces/services/service.go
+++ internal/interfaces/services/service.go
@@ -2,14 +2,7 @@ package services
 
 import (
 	"context"
-	"time"
 )
-
-type InputLink struct {
-	Link      string
-	FakeLink  string
-	EraseTime time.Time
-}
 
 //go:generate go run github.com/vektra/mockery/v2@v2.20.2 --name=Service --output=../../../mocks
 type Service interface {
