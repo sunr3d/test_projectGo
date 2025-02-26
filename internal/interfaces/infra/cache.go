75098d669e82841fe1012636938e67cff383f1c5
Sunred <sunr3d.coding@gmail.com>
2025-02-26 21:11:33 +0300

    feat: интерфейс под кэш

---
+++ internal/interfaces/infra/cache.go
@@ -0,0 +1,10 @@
+package infra
+
+import "context"
+
+//go:generate go run github.com/vektra/mockery/v2@v2.20.2 --name=Cache --output=../../../mocks
+type Cache interface {
+	Get(ctx context.Context, key string) (string, error)
+	Set(ctx context.Context, key string, value any) error
+	Close() error
+}
