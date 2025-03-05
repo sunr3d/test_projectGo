bec4e72a13982735a4ddd188dd2a0dce97535dc7
Sunred <sunr3d.coding@gmail.com>
2025-03-05 16:01:19 +0300

    feat: google/api proto_libs

---
+++ internal/gateway/gateway.go
@@ -0,0 +1,40 @@
+package gateway
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pbls "link_service/proto/link_service"
+)
+
+type Gateway struct {
+	logger *zap.Logger
+}
+
+func New(logger *zap.Logger) *Gateway {
+	return &Gateway{
+		logger: logger,
+	}
+}
+
+func (g *Gateway) Run(ctx context.Context, grpcPort, httpPort string) error {
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+
+	if err := pbls.RegisterLinkServiceHandlerFromEndpoint(ctx, mux, fmt.Sprintf("localhost:%s", grpcPort), opts); err != nil {
+		return fmt.Errorf("gateway.Run, failed to register handler: %w", err)
+	}
+
+	g.logger.Info("HTTP server started", zap.String("address", fmt.Sprintf("localhost:%s", httpPort)))
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", httpPort), mux); err != nil {
+		return fmt.Errorf("gateway.Run, failed server HTTP: %w", err)
+	}
+
+	return nil
+}
