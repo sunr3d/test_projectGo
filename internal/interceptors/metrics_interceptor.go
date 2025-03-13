2f2a14f35f498219824bf2f7618f5165feceda58
Sunred <sunr3d.coding@gmail.com>
2025-03-13 22:12:23 +0300

    chore: defer RequestDuration() metric

--- internal/interceptors/metrics_interceptor.go
+++ internal/interceptors/metrics_interceptor.go
@@ -9,21 +9,23 @@ import (
 	"link_service/internal/server/metrics"
 )
 
-// MetricsUnaryInterceptor перехватывает выполнение метода и собирает метрикиере
+// MetricsUnaryInterceptor перехватывает выполнение метода и собирает метрики
 func MetricsUnaryInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
-		start := time.Now()
 		method := info.FullMethod // вытаскивает имя метода который вызывается
+		start := time.Now()
 
 		// Увеличиваем значение счетчика вызова указанного метода на 1
 		metrics.RequestCount.WithLabelValues(method).Inc()
 
+		// Считаем длительность запроса после того как интерцептор отработал
+		defer func() {
+			duration := time.Since(start).Seconds()
+			metrics.RequestDuration.WithLabelValues(method).Observe(duration)
+		}()
+
 		// Непосредственно выполняем вызванный метод внутри нашего интерцептора
 		resp, err := handler(ctx, req)
-
-		// Считаем длительность запроса
-		duration := time.Since(start).Seconds()
-		metrics.RequestDuration.WithLabelValues(method).Observe(duration)
 
 		// Если запрос завершился с ошибкой, увеличиваем значение счетчика ошибок на 1
 		if err != nil {
