766941ae2baece1e7b78174da9da20f4b5bb1b87
Sunred <sunr3d.coding@gmail.com>
2025-03-13 21:19:43 +0300

    chore: Добавлена инициализация и подъем сервера для Метрик. Так же обновлен метод Stop(), ожидание 0.5 сек позволяет сигналу контекста зашатдаунить зависимые сервера

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,7 +3,10 @@ package server
 import (
 	"context"
 	"fmt"
+	"link_service/internal/config"
+	"link_service/internal/server/metrics"
 	"net"
+	"time"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -12,25 +15,27 @@ import (
 )
 
 type Server struct {
-	Server        *grpc.Server
-	logger        *zap.Logger
-	GRPCAddress   string
-	HTTPAddress   string
-	GatewayEnable bool
-	ctx           context.Context
-	cancel        context.CancelFunc
+	Server         *grpc.Server
+	logger         *zap.Logger
+	GRPCAddress    string
+	HTTPAddress    string
+	PrometheusAddr string
+	GatewayEnable  bool
+	ctx            context.Context
+	cancel         context.CancelFunc
 }
 
-func New(logger *zap.Logger, GRPCPort, HTTPPort string, GatewayEnable bool) *Server {
+func New(logger *zap.Logger, cfg *config.Config) *Server {
 	ctx, cancel := context.WithCancel(context.Background())
 	return &Server{
-		Server:        grpc.NewServer(),
-		logger:        logger,
-		GRPCAddress:   fmt.Sprintf("localhost:%s", GRPCPort),
-		HTTPAddress:   fmt.Sprintf("localhost:%s", HTTPPort),
-		GatewayEnable: GatewayEnable,
-		ctx:           ctx,
-		cancel:        cancel,
+		Server:         grpc.NewServer(),
+		logger:         logger,
+		GRPCAddress:    fmt.Sprintf("localhost:%s", cfg.GRPCPort),
+		HTTPAddress:    fmt.Sprintf("localhost:%s", cfg.HTTPPort),
+		PrometheusAddr: fmt.Sprintf("localhost:%s", cfg.Prometheus.Port),
+		GatewayEnable:  cfg.GatewayEnable,
+		ctx:            ctx,
+		cancel:         cancel,
 	}
 }
 
@@ -40,9 +45,21 @@ func (s *Server) Run() error {
 		return fmt.Errorf("failed to listen on address %s: %w\n", s.GRPCAddress, err)
 	}
 
-	s.logger.Info("gRPC server start",
+	s.logger.Info("server.Run: gRPC server started",
 		zap.String("address", s.GRPCAddress),
 	)
+
+	go func() {
+		mtr := metrics.New(s.logger)
+		if err = mtr.Init(); err != nil {
+			s.logger.Error("metrics.Init: ", zap.Error(err))
+			return
+		}
+
+		if err = mtr.Run(s.ctx, s.PrometheusAddr); err != nil {
+			s.logger.Error("metrics.Run: ", zap.Error(err))
+		}
+	}()
 
 	if s.GatewayEnable {
 		go func() {
@@ -61,7 +78,9 @@ func (s *Server) Run() error {
 }
 
 func (s *Server) Stop() {
-	s.logger.Info("gRPC server stop")
+	s.logger.Info("gRPC server STOP signal")
 	s.cancel()
+	time.Sleep(500 * time.Millisecond)
 	s.Server.GracefulStop()
+	s.logger.Info("gRPC server stopped")
 }
