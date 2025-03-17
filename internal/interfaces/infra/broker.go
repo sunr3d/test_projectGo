cb783bd571a7aca24f8d66e9c34a03b8a37901b2
Sunred <sunr3d.coding@gmail.com>
2025-03-17 18:10:52 +0300

    chore: go generate added for mockery

--- internal/interfaces/infra/broker.go
+++ internal/interfaces/infra/broker.go
@@ -2,6 +2,7 @@ package infra
 
 import "context"
 
+//go:generate go run github.com/vektra/mockery/v2@v2.53.2 --name=Broker --output=../../../mocks
 type Broker interface {
 	Add(ctx context.Context, topic string, message []byte) error
 }
