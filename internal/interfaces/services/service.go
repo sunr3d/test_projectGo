14440e8f91b5a66f9ce047c59c606099ed93b438
Sunred <sunr3d.coding@gmail.com>
2025-03-17 18:10:24 +0300

    chore: метод AddMessage

--- internal/interfaces/services/service.go
+++ internal/interfaces/services/service.go
@@ -2,10 +2,12 @@ package services
 
 import (
 	"context"
+	"github.com/segmentio/kafka-go"
 )
 
 //go:generate go run github.com/vektra/mockery/v2@v2.53.2 --name=Service --output=../../../mocks
 type Service interface {
 	Create(ctx context.Context, link InputLink) error
 	Find(ctx context.Context, fakeLink string) (string, error)
+	AddMessage(ctx context.Context, msg kafka.Message) error
 }
