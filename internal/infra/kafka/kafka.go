f2435d11b0ae098de442c59699505c1623033a38
Sunred <sunr3d.coding@gmail.com>
2025-03-21 19:41:28 +0300

    chore: full kafka init method!

--- internal/infra/kafka/kafka.go
+++ internal/infra/kafka/kafka.go
@@ -2,6 +2,8 @@ package kafka_impl
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 	"go.uber.org/zap"
@@ -13,24 +15,42 @@ var _ infra.Broker = (*Kafka)(nil)
 
 type Kafka struct {
 	Writer *kafka.Writer
+	Conn   *kafka.Conn
 	Logger *zap.Logger
 }
 
-func New(log *zap.Logger, port string) infra.Broker {
+func New(log *zap.Logger, port string) (infra.Broker, error) {
 	writer := kafka.Writer{
 		Addr:     kafka.TCP("localhost:" + port),
 		Topic:    "link_service",
 		Balancer: &kafka.LeastBytes{},
 	}
+
+	conn, err := kafka.Dial("tcp", "localhost:"+port)
+	if err != nil {
+		return nil, fmt.Errorf("kafka_impl.New: %w", err)
+	}
+
+	err = conn.SetDeadline(time.Now().Add(5 * time.Second))
+	if err != nil {
+		return nil, fmt.Errorf("kafka_impl.New: %w", err)
+	}
+
+	if _, err = conn.Brokers(); err != nil {
+		return nil, fmt.Errorf("kafka_impl.New: %w", err)
+	}
 	log.Info("Connect to Kafka success")
 
-	return &Kafka{Writer: &writer, Logger: log}
+	return &Kafka{Writer: &writer, Conn: conn, Logger: log}, nil
 }
 
-func (k *Kafka) Add(ctx context.Context, topic string, message []byte) error {
-	return k.Writer.WriteMessages(ctx, kafka.Message{Topic: topic, Value: message})
+func (k *Kafka) Add(ctx context.Context, topic string, key []byte, message []byte) error {
+	return k.Writer.WriteMessages(ctx, kafka.Message{Topic: topic, Key: key, Value: message})
 }
 
 func (k *Kafka) Close() error {
-	return k.Writer.Close()
+	if err := k.Writer.Close(); err != nil {
+		return fmt.Errorf("kafka_impl.Close: %w", err)
+	}
+	return k.Conn.Close()
 }
